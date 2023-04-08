6a190fac2eaa17089114540b1e2393698a20a4a4
Richard Baker <thefibonaccisolution@gmail.com>
2023-04-08 18:47:26 +0100

    First Commit

---
+++ TEKAPrototype/Test/TestClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEKAPrototype.Test
+{
+    public class TestClass
+    {
+        #region Variables
+
+        #endregion Variables
+
+        #region Constructor
+
+        #endregion Constructor
+
+        #region Methods
+
+        #endregion Methods
+
+        #region Events
+
+        #endregion Events
+    }
+}
